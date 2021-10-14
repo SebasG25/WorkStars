cfe35690de9b1c2227a8ce6d485fabfdca6473a9
Sebastian Guzman <bitas200225@gmail.com>
2021-10-14 14:46:47 -0500

    Pequeños detalles sprint 1

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -21,8 +21,8 @@ export default function Signin() {
             <section className="login py-5">
                 <div className="container">
                     <div className="g-0 row row-signin">
-                        <div className="col-lg-5 col-md-5 col-sm-5">
-                            <img className="img-fluid" src="https://images4.alphacoders.com/100/thumb-1920-1009824.jpg" alt="" />
+                        <div className="col-lg-5 col-md-5 col-sm-5 row-image">
+                            <img className="img-fluid" src="https://i.imgur.com/hriWgYh.jpg" alt="" />
                         </div>
                         <div className="col-lg-7 col-md-7 col-sm-7 text-center py-5">
                             <h1 className="animate__animated animate__tada">Welcome Back</h1>
